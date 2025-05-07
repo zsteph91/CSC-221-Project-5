458128ca91680eda036a5e6f9ee14280459be413
Zach Stephan <zas5035@gmail.com>
2025-05-07 02:15:13 -0400

    Patterns

--- CSC 221 Project 5.cpp
+++ CSC 221 Project 5.cpp
@@ -1,4 +1,4 @@
-// CSC 221 Project 5.cpp : Program 3: Population Bar Chart
+// CSC 221 Project 5.cpp : Program 4: Patterns
 
 #include <iostream>
 #include <iomanip>
@@ -9,41 +9,28 @@ using namespace std;
 
 int main()
 {
-    // produces a bar chart for the population growth of a town
-    // The data is stored in intervals over the past several years, with the year and the population for that year 
-    // (rounded to the nearest 1,000 people) in each line of the file
-    // ask for the name of the town and the name of the data file
-    // open the file and verify that it has opened with no errors
-    // Then the program should read the year and the population figure and display the year and population on a bar chart
-    // For each year, it should display the year and a bar consisting of one asterisk for each 1,000 people
+    // create two patterns of "+"s in the shape of a triangle and inverted triangle next to one another
 
-    string townName, fileName, stars;
-    int population, numStars, year = 1900;
+    int rows;
 
-    cout << "What is the name of your town? ";
-    cin >> townName;
-    cout << "What is the name of your file? ";
-    cin >> fileName;
+    cout << "How many rows would you like? ";
+    cin >> rows;
 
-    cout << townName << "'s Population Growth:" << endl;
-    cout << "(each * represents 1,000 people)" << endl;
-
-    ifstream inFile(fileName);
-    if (!inFile) {
-        cerr << "Error opening the file " << fileName << endl;
-        return 1;
-    }
-    while (inFile >> population) {
-        cout << year << " ";
-        year += 20;
-        numStars = population / 1000;
-        for (int i = 0; i < numStars; i++) {
-            cout << "*";
+    for (int a = 1; a <= rows; a++) {
+        for (int i = 1; i <= a; i++) {
+            cout << "+";
         }
         cout << endl;
     }
+    
+    cout << endl;
 
-    inFile.close();
+    for (int b = rows; b >= 1; b--) {
+        for (int j = 1; j <= b; j++) {
+            cout << "+";
+        }
+        cout << endl;
+    }
 }
 
 
