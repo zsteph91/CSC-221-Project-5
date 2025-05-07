abed3c8813b2d0812ea35f4951d47044c99e90e8
Zach Stephan <zas5035@gmail.com>
2025-05-07 02:42:11 -0400

    Random Number Guessing Game

--- CSC 221 Project 5.cpp
+++ CSC 221 Project 5.cpp
@@ -1,36 +1,46 @@
-// CSC 221 Project 5.cpp : Program 4: Patterns
+// CSC 221 Project 5.cpp : Program 5: Random Number Guessing Game
 
 #include <iostream>
 #include <iomanip>
-#include <fstream>
-#include <iostream>
+#include <random>
 #include <string>
 using namespace std;
 
 int main()
 {
-    // create two patterns of "+"s in the shape of a triangle and inverted triangle
+    // generates a random number and asks the user to guess what the number is
+    // if the user’s guess is higher than the random number, the program should display “Too high, try again.”
+    // If the user’s guess is lower than the random number, the program should display “Too low, try again.”
 
-    int rows;
+    int upperLimit, randNum, guess, counter = 0;
 
-    cout << "How many rows would you like? ";
-    cin >> rows;
+    cout << "What range of numbers would you like to guess from? 1 - (largest number you want): ";
+    cin >> upperLimit;
 
-    for (int a = 1; a <= rows; a++) {
-        for (int i = 1; i <= a; i++) {
-            cout << "+";
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution <> dist(1, upperLimit);
+    randNum = dist(gen);
+
+    do {
+        counter++;
+
+        cout << "What is your guess? ";
+        cin >> guess;
+
+        if (guess == randNum) {
+            cout << "Total guesses: " << counter;
         }
-        cout << endl;
-    }
-    
-    cout << endl;
 
-    for (int b = rows; b >= 1; b--) {
-        for (int j = 1; j <= b; j++) {
-            cout << "+";
+        else if (guess > randNum) {
+            cout << "Too high, try again. ";
         }
-        cout << endl;
-    }
+
+        else {
+            cout << "Too low, try again. ";
+        }
+
+    } while (guess != randNum);
 }
 
 
