d76500300cb04ff27349a0aa457b20d56bded7a4
Zach Stephan <zas5035@gmail.com>
2025-05-07 01:07:37 -0400

    Population Bar Chart

--- CSC 221 Project 5.cpp
+++ CSC 221 Project 5.cpp
@@ -1,37 +1,49 @@
-// CSC 221 Project 5.cpp : Program 2: Student Lineup
+// CSC 221 Project 5.cpp : Program 3: Population Bar Chart
 
 #include <iostream>
 #include <iomanip>
 #include <fstream>
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    // read student names from the file LineUp.txt
-    // output the count of the number of students in the file
-    // output which student will be at the front of the alphabetically organized line
-    // output which student will be at the end of that line
+    // produces a bar chart for the population growth of a town
+    // The data is stored in intervals over the past several years, with the year and the population for that year 
+    // (rounded to the nearest 1,000 people) in each line of the file
+    // ask for the name of the town and the name of the data file
+    // open the file and verify that it has opened with no errors
+    // Then the program should read the year and the population figure and display the year and population on a bar chart
+    // For each year, it should display the year and a bar consisting of one asterisk for each 1,000 people
 
-    ifstream inFile("LineUp.txt");
+    string townName, fileName, stars;
+    int population, numStars, year = 1900;
+
+    cout << "What is the name of your town? ";
+    cin >> townName;
+    cout << "What is the name of your file? ";
+    cin >> fileName;
+
+    cout << townName << "'s Population Growth:" << endl;
+    cout << "(each * represents 1,000 people)" << endl;
+
+    ifstream inFile(fileName);
     if (!inFile) {
-        cerr << "Error opening file" << endl;
+        cerr << "Error opening the file " << fileName << endl;
         return 1;
     }
-
-    string name, first = "z", last = "A";
-    int count = 0; // variable to count students in the file
-    while (inFile >> name) {
-        if (first > name) {
-            first = name;
+    while (inFile >> population) {
+        cout << year << " ";
+        year += 20;
+        numStars = population / 1000;
+        for (int i = 0; i < numStars; i++) {
+            cout << "*";
         }
-        if (last < name) {
-            last = name;
-        }
-        count += 1;
+        cout << endl;
     }
-    cout << "There are " << count << " students in the class." << endl << first << " will be at the front of the line, and " << last 
-        << " will be at the end of the line." << endl;
+
+    inFile.close();
 }
 
 
